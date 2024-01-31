53148dd9b211784bc652c45227d59035b3401279
khalym <aerys.prog@gmail.com>
2024-01-31 23:08:17 +0200

    init project

---
+++ src/test/java/com/khalore/SnapWodsBackend/SnapWodsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalore.SnapWodsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapWodsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
